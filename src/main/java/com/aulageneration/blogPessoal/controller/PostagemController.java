1ff3987778128368195b99bdc84075feafde699f
Maria Luísa <malumaricatoo@gmail.com>
2021-05-27 19:30:08 -0300

    Blog Pessoal Atualização 2

--- src/main/java/com/aulageneration/blogPessoal/controller/PostagemController.java
+++ src/main/java/com/aulageneration/blogPessoal/controller/PostagemController.java
@@ -3,10 +3,13 @@ package com.aulageneration.blogPessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,4 +43,9 @@ public class PostagemController {
 		
 	}
 
-}
+	@PostMapping
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+			}
+
