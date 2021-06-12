927076b93dae5140bfd2d3fe1e69bf701e17441b
Maria Luísa <malumaricatoo@gmail.com>
2021-06-12 19:11:23 -0300

    Deploy no Heroku

--- src/main/java/com/aulageneration/blogPessoal/BlogPessoalApplication.java
+++ src/main/java/com/aulageneration/blogPessoal/BlogPessoalApplication.java
@@ -3,9 +3,13 @@ package com.aulageneration.blogPessoal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class BlogPessoalApplication {
 	
 	@GetMapping
